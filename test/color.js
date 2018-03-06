f4ee1b89ca24dd792152d675952272347dad9cf8
Nick Buhay <nbuhay@gmail.com>
2018-03-06 03:51:58 -0600

    Add Mocha test for https req.

--- test/color.js
+++ test/color.js
@@ -1,13 +1,71 @@
-const chai = require('chai')
+const https = require('https')
+    , chai = require('chai')
+    , assert = chai.assert
     , should = chai.should()
-    , color = require('color');
+    , sinon = require('sinon')
+    , nock = require('nock')
+    , __test = require('color').__testonly__
 
 describe('color.js', () => {
 
-  describe('#spotifyGetCurrentlyPlaying', () => {
-
-    it('should exist', () => should.exist(color.__testonly__.spotifyGetCurrentlyPlaying));
-    
+  beforeEach(() => {
+    SPOTIFY_ID = process.env.SPOTIFY_ID
+    SPOTIFY_SECRET = process.env.SPOTIFY_SECRET
+    SPOTIFY_REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN
+    sandbox = sinon.sandbox.create()
   });
 
-});
+  afterEach(() => sandbox.restore());
+
+  describe('#spotifyRefreshToken', () => {
+
+    it('should exist', () => should.exist(__test.spotifyRefreshToken))
+
+    it('POST https://accounts.spotify.com/api/token', () => {
+      const basic_auth = `${SPOTIFY_ID}:${SPOTIFY_SECRET}`
+          , options = {
+              hostname: 'accounts.spotify.com',
+              method: 'POST',
+              path: '/api/token',
+              headers: {
+                'Authorization': `Basic ${new Buffer(basic_auth).toString('base64')}`,
+                'Content-Type':  'application/x-www-form-urlencoded',
+              }
+          }
+          , dummyResponse = { access_token: 'SPOTIFY_DUMMY_ACCESS_TOKEN' }
+
+      let httpsReqSpy = sandbox.spy(https, 'request')
+
+      nock('https://accounts.spotify.com')
+        .post('/api/token')
+        .reply(200, dummyResponse)
+
+      return __test.spotifyRefreshToken()
+        .then(() => {
+          assert(httpsReqSpy.calledOnce);
+
+          // spy called with options as args
+          let spyPassedArgs = httpsReqSpy.firstCall.args[0];
+
+          Object.keys(options).forEach((key) => {
+            should.exist(spyPassedArgs[key]);
+            if (key === 'headers') {
+              Object.keys(options.headers).forEach((header) => {
+                options.headers[header].should.equal(spyPassedArgs.headers[header]);
+              })
+            } else {
+              options[key].should.equal(spyPassedArgs[key]);
+            }
+          })
+        })
+    })
+    
+  })
+
+  describe('#spotifyGetCurrentlyPlaying', () => {
+
+    it('should exist', () => should.exist(__test.spotifyGetCurrentlyPlaying));
+    
+  })
+
+})
